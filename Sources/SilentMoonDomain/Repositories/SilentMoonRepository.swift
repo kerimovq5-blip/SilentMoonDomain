0ef6f700a755c379de7b95c842ad0cd1938ef69c
Qehreman <kerimovq5@gmail.com>
2026-08-17 18:59:59 +0400

    Repository Protocol

---
+++ Sources/SilentMoonDomain/Repositories/SilentMoonRepository.swift
@@ -0,0 +1,34 @@
+//
+//  SilentMoonRepository.swift
+//  SilentMoonDomain
+//
+//  Created by Kerimov Qehreman on 17.08.26.
+//
+
+import Foundation
+
+public protocol SilentMoonRepository: Sendable {
+    func register(name: String, email: String, password: String) async -> Result<RegisterResponseEntity, Error>
+    
+    func login(email: String, password: String) async -> Result<AuthResponseEntity, Error>
+    
+    func verifyEmail(email: String, otp: String) async -> Result<AuthResponseEntity, Error>
+    
+    func resendOtp(email: String) async -> Result<ResendOtpResponseEntity, Error>
+    
+    func googleLogin(idToken: String) async -> Result<AuthResponseEntity, Error>
+    
+    func forgotPassword(email: String) async -> Result<SimpleMessageResponseEntity, Error>
+    
+    func resetPassword(email: String, otp: String, newPassword: String) async -> Result<SimpleMessageResponseEntity, Error>
+    
+    func refreshToken() async -> Result<AuthResponseEntity, Error>
+    
+    func logout() async -> Result<Void, Error>
+    
+    func search(query: String, type: String?, page: Int, limit: Int) async -> Result<SearchResponseEntity, Error>
+    
+    func getTopics() async -> Result<[String], Error>
+    
+    func updateTopics(topicIds: [String]) async -> Result<[String], Error>
+}
